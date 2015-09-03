5a5016a7521b828ddaf244f0f1f8c9bf01d950d4
Pato Sainz <pato@printoo.me>
2015-09-02 19:35:36 -0500

    exit

--- index.php
+++ index.php
@@ -109,4 +109,12 @@ var google_remarketing_only = false;
 
 <?php include ("includes/footer.php");?>
 </body>
+<script type="text/javascript">
+    (function(a,e,f,g,b,c,d){a[b]=a[b]||function(){
+      (a[b].q=a[b].q||[]).push(arguments)};
+      c=e.createElement(f);d=e.getElementsByTagName(f)[0];c.async=1;c.src=g;
+      d.parentNode.insertBefore(c,d)
+    })(window,document,"script","//cdn.app.exitmonitor.com/em.js","em");
+
+    em('set', 'campaign', '55e791bc3936310017050000');
 </html>
