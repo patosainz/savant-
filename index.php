dd6e29dd234d927e73719b619632b23fa96660c9
Pato Sainz <pato@printoo.me>
2015-10-13 11:50:43 -0500

    código de rastreo dde llamadas google

--- index.php
+++ index.php
@@ -30,6 +30,41 @@ document.readyState != "complete" ? (window.attachEvent ? window.attachEvent('on
 <body>
 <?php include ("includes/header.php");?>
 
+<!-- Google Code for Llamadas Savant Conversion Page
+In your html page, add the snippet and call
+goog_report_conversion when someone clicks on the
+phone number link or button. -->
+<script type="text/javascript">
+  /* <![CDATA[ */
+  goog_snippet_vars = function() {
+    var w = window;
+    w.google_conversion_id = 943675560;
+    w.google_conversion_label = "OgI7CO3_4l8QqLH9wQM";
+    w.google_remarketing_only = false;
+  }
+  // DO NOT CHANGE THE CODE BELOW.
+  goog_report_conversion = function(url) {
+    goog_snippet_vars();
+    window.google_conversion_format = "3";
+    window.google_is_call = true;
+    var opt = new Object();
+    opt.onload_callback = function() {
+    if (typeof(url) != 'undefined') {
+      window.location = url;
+    }
+  }
+  var conv_handler = window['google_trackConversion'];
+  if (typeof(conv_handler) == 'function') {
+    conv_handler(opt);
+  }
+}
+/* ]]> */
+</script>
+<script type="text/javascript"
+  src="//www.googleadservices.com/pagead/conversion_async.js">
+</script>
+
+
 <section id="MainSlide"></section>
 <section id="TratamientosAlopecia" class="WhiteContent warp">
 	<article>
