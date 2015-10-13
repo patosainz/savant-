045e41ff65002b74d523561bd88c7e7046a6e5d7
Pato Sainz <pato@printoo.me>
2015-10-13 12:41:41 -0500

    fix mail add phpmailer class

--- contacto.php
+++ contacto.php
@@ -12,6 +12,7 @@ if (isset($_GET["send"])) {
 	$asunto = "Contacto Sitio Savant";
 	$mensaje = '<table width="500" border="0" align="center" cellpadding="3" cellspacing="0" style="font-family:Calibri, Arial, sans-serif; font-size:14px;"><tr><td colspan="2" align="left" bgcolor="#3F464E" style="padding:20px 10px;"><img src="http://savant.com.mx/imagenes/savant-logo-simple.png" width="175" height="29" alt=""></td></tr><tr><td width="85" align="right" bgcolor="#F3F3F3" style="font-size:11px; color:#999; padding-right:10px;">NOMBRE</td><td width="403" bgcolor="#F3F3F3">'.$Nombre.'</td></tr><tr><td width="85" align="right" bgcolor="#F3F3F3" style="font-size:11px; color:#999; padding-right:10px;">E-MAIL</td><td width="403" bgcolor="#F3F3F3">'.$FromMail.'</td></tr><tr><td width="85" align="right" bgcolor="#F3F3F3" style="font-size:11px; color:#999; padding-right:10px;">TELEFONO</td><td width="403" bgcolor="#F3F3F3">'.$Telefono.'</td></tr><tr><td align="right" bgcolor="#F3F3F3" style="font-size:11px; color:#999; padding-right:10px;">MEDIO CONTAC</td><td bgcolor="#F3F3F3">'.$Como.'</td></tr><tr><td colspan="2" bgcolor="#FFFFFF" style="padding:15px;"><span style="font-size:11px; color:#999;">COMENTARIOS</span><br>'.$Mensaje.'</td></tr></table>';
 
+  $mail = new PHPMailer;
 	$mail->IsSMTP();
   $mail->Host = 'smtp.mandrillapp.com';                 // Specify main and backup server
   $mail->Port = 587;                                    // Set the SMTP port
